b0da0833e6a7c8ecc5ce596206cde0420958260d
kokuden72-dot <kokuden72@gmail.com>
2026-06-29 19:47:51 +0900

    Final revision

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <math.h>
 
 int main() {
+    char username[100];
+    printf("Who are you?\n");
+    scanf("%s", username);
+    printf("Hello, %s!\n", username);
     int count = 0;
     printf("Tossing a coin...\n");
     printf("Round 1: ");
@@ -27,5 +31,10 @@ int main() {
         printf("Tails\n");
     }
     printf("Heads: %d, Tails: %d\n", count, 3 - count);
+    if(count<2) {
+        printf("%s lose!\n", username);
+    } else {
+        printf("%s win!\n", username);
+    }
     return 0;
 }
